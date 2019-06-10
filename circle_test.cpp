7c3bbf26b143e2b956ad2a085df9f4f4033c760a
David Murray <davemurray9060@gmail.com>
2019-06-10 02:06:01 -0500

    Add files via upload

--- circle_test.cpp
+++ circle_test.cpp
@@ -14,8 +14,8 @@ extern double InputSignal_f32_1kHz_15kHz[320];
 using namespace std;
 
 int bufptr = 0;
-double inputbuffer[256];
-double outputbuffer[256];
+double inputbuffer[345] = { 0 };
+double outputbuffer[345] = { 0 };
 int bit_depth = 32;
 double max;
 
@@ -34,6 +34,7 @@ double release = 0.005;
 double scaling_factor;
 double exp_scale;
 double gain;
+double level = 10;
 
 
 int main(void) {
@@ -45,17 +46,20 @@ int main(void) {
 	Effect2.setFc(440);
 	Effect2.setFs(300);
 	*/
-	circular_buffer<uint32_t> circle(bit_depth*size(InputSignal_f32_1kHz_15kHz));
-	while (1) {
-		//getline(cin, Effect);
-		circle.put(InputSignal_f32_1kHz_15kHz[bufptr]);		// load buffer
-		inputbuffer[bufptr] = static_cast<double>(circle.get_head());
+	circular_buffer<double> circle(345);
+	while (bufptr < 345) {
+		gain = 2;
+		circle.put(InputSignal_f32_1kHz_15kHz[bufptr]);		// load buffer	
 		// linearly normalize input
-		inputbuffer[bufptr] = inputbuffer[bufptr] / *max_element(begin(inputbuffer), end(inputbuffer));
-
+		inputbuffer[bufptr] = (circle.get_head()); 
+		if (bufptr > 30) {
+			inputbuffer[bufptr] = (inputbuffer[bufptr]) / (*max_element(begin(inputbuffer), end(inputbuffer)) * level);
+		}
+		cout << "input: " << inputbuffer[bufptr] << endl;
 		switch (Effect) {
 		case 1:
-			Effect1.process_samples(inputbuffer, outputbuffer, bufptr);
+			Effect1.process_samples(&inputbuffer[0], &outputbuffer[0], bufptr);
+			cout << "effect output: " << outputbuffer[bufptr] << endl;
 			break;
 		case 2:
 			// Hifi.write(Effect2.process_samples(circle.get());
@@ -69,17 +73,17 @@ int main(void) {
 /*
  |     ,.---..._ | :::  -attack |
  |   ,`:: / / / o'''.| ///  - sustain |
- |  ' :::/ / / / oooooo`'..| ooo - release |
+ |  , ::::/ / / / oooooo`'..| ooo - release |
  | ,::::: / / / ooooooooo`''.| ________________ |
  | _:::::/ / / / oooooooooooo`'''.____________________time
 */
 		rms_amplitude = (1 - rms_width) * rms_amplitude + (rms_width * outputbuffer[bufptr]);
-		rms_dB = 10 * log(rms_amplitude); // Hifi.write(Effect2.process_samples(circle.get());
+		rms_dB = 10 * log10(rms_amplitude); // Hifi.write(Effect2.process_samples(circle.get());
 		// positve comp slope
 		comp_scale = comp_slope * (comp_threshold - rms_dB);
 		// negative comp slope
 		exp_scale = exp_slope * (exp_threshold - rms_dB);
-		min_amplitude = *min_element(begin(outputbuffer), end(outputbuffer));
+		min_amplitude = *min_element(begin(inputbuffer), end(inputbuffer));
 		scaling_factor = pow(10, min_amplitude);
 		if (scaling_factor < gain) {
 			gain = (1 - attack) * gain + (attack * scaling_factor);
@@ -87,7 +91,10 @@ int main(void) {
 			gain = (1 - release) * gain + (release * scaling_factor);
 		}
 		outputbuffer[bufptr] = gain * outputbuffer[bufptr];
-		cout << outputbuffer[bufptr] << endl;
+		cout << "output: " << outputbuffer[bufptr] << endl;
+		circle.put(outputbuffer[bufptr]); 
+		cout << "get_head: " << circle.get_head() << endl;
+		cout << "get_head to head ratio: " << circle.get_head() / circle.get() << endl;
 		bufptr++;
 	}
 }
