f6c57727512237462fbb40f643d4bcf606f1da59
David Murray <davemurray9060@gmail.com>
2019-10-07 01:59:36 -0500

    Update ringModulator.h

--- I2S.ino/ringModulator.h
+++ I2S.ino/ringModulator.h
@@ -7,14 +7,12 @@
 
 class RingModulator : public Effect
 {
+private:
+  float Fc = 440;
+  float Fs = 300;
+  int bypass = 1;
 public:
   void setFs(float Fs);
   void setFc(float Fc);
   void process_samples(float *input, float *output);
-  float Fc = 440;
-  float Fs = 300;
-  int bypass = 1;
-
-private:
-
 };
