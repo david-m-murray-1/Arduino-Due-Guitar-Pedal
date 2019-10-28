6d009e208050521bb284b83545501839b07e09eb
David Murray <davemurray9060@gmail.com>
2019-10-28 01:20:50 -0500

    Update delay.h

--- circle_test/Effects/delay.h
+++ circle_test/Effects/delay.h
@@ -1,22 +1,19 @@
-#include "effect.h"
+#include "Effect.h"
 
 #define DELAYBUFFERSIZE 44100
 
 class Delay : public Effect
 {
-public:
-  //functions
-  void setDelayTime(float delayTime);
-  void setFeedback(float feedback);
-  void process_samples(float *inputbuffer, float *outputbuffer);
-
-  float delayBuffer[DELAYBUFFERSIZE];
+private:
   int delayTime = 5000;
   int input = 0;
   int output = 0;
   float feedback = 0.5;
-  int bypass = 1;
+public:
+  //functions
+  void setDelayTime(float delayTime);
+  void setFeedback(float feedback);
+  void process_samples(float *inputbuffer, float *outputbuffer, int bufptr);
+  float delayBuffer[DELAYBUFFERSIZE]; // 
 
-private:
-
-}; // Delay
+};
