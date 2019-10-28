f486321924a447e1afc46afc8fd010efb30dccb5
David Murray <davemurray9060@gmail.com>
2019-10-28 01:18:30 -0500

    Update ringModulator.h

--- circle_test/Effects/ringModulator.h
+++ circle_test/Effects/ringModulator.h
@@ -1,20 +1,15 @@
-#include "effect.h"
+#include "Effect.h"
 #include <math.h>
 #include <complex.h>
 #include <iostream>
 
-
-
 class RingModulator : public Effect
 {
+private:
+    float Fc = 440;
+    float Fs = 300; 
 public:
   void setFs(float Fs);
   void setFc(float Fc);
-  void process_samples(float *input, float *output);
-  float Fc = 440;
-  float Fs = 300;
-  int bypass = 1;
-
-private:
-
+  void process_samples(float *input, float *output, int bufptr);
 };
