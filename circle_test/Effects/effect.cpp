32ca23a0cfa1dfb214970dcbaef4528617d045dd
David Murray <davemurray9060@gmail.com>
2019-10-27 21:15:03 -0500

    Update effect.cpp

--- circle_test/Effects/effect.cpp
+++ circle_test/Effects/effect.cpp
@@ -12,7 +12,7 @@ Effect::~Effect()
 } // ~Effect()
 
 
-void Effect::process_samples(float *inputbuffer,float *outputbuffer)
+void Effect::process_samples(float *inputbuffer,float *outputbuffer, int bufptr)
 {
   for(int i=0; i<FRAMESPERBUFFER; i++) outputbuffer[i]=inputbuffer[i];
 }
