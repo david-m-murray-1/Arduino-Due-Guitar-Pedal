6fd68fe31c1cc9ed9b062c1928aaa105b1cdf122
David Murray <davemurray9060@gmail.com>
2019-12-02 04:11:11 -0600

    Update distortion.cpp

--- circle_test/Effects/distortion.cpp
+++ circle_test/Effects/distortion.cpp
@@ -10,14 +10,20 @@ void Distortion::setDepth(double depth){
 }
 
 //processing samples
-double Distortion::process_samples(double *inputbuffer, double *outputbuffer, int bufptr)
-{
-	inputbuffer[bufptr] = inputbuffer[bufptr] * depth;
-	inputbuffer[bufptr] = tanh((inputbuffer[bufptr] * (timbre + 1)));
-	inputbuffer[bufptr] = (inputbuffer[bufptr] * ((0.1 + timbre) * timbreInverse));
-	inputbuffer[bufptr] = cos((inputbuffer[bufptr] + (timbre + 0.25)));
-	inputbuffer[bufptr] = tanh(inputbuffer[bufptr] * (timbre + 1));
-	inputbuffer[bufptr] = inputbuffer[bufptr] * 0.125;
-	outputbuffer[bufptr] = inputbuffer[bufptr];
-	return *outputbuffer;
+double Distortion::process_samples(double *inputbuffer, double *outputbuffer, int bufptr, int mode)
+	switch(mode){
+		Case 1:	//linear_distortion
+			inputbuffer[bufptr] = inputbuffer[bufptr] * depth;
+			inputbuffer[bufptr] = tanh((inputbuffer[bufptr] * (timbre + 1)));
+			inputbuffer[bufptr] = (inputbuffer[bufptr] * ((0.1 + timbre) * timbreInverse));
+			inputbuffer[bufptr] = cos((inputbuffer[bufptr] + (timbre + 0.25)));
+			inputbuffer[bufptr] = tanh(inputbuffer[bufptr] * (timbre + 1));
+			inputbuffer[bufptr] = inputbuffer[bufptr] * 0.125;
+			outputbuffer[bufptr] = inputbuffer[bufptr];
+			return outputbuffer[bufptr];
+			break;
+		Case 2:	// Non-linear distortion
+			outputbuffer[bufptr] = (inputbuffer[bufptr]/fabs(inputbuffer[bufptr])) * (1-exp((alpha*(inputbuffer[bufptr]*inputbuffer[bufptr])/fabs(inputbuffer[bufptr]))));
+			break;			
+	}
 }
